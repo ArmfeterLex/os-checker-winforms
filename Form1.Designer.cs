55f505b2ceb64093c2b0a7dcc8c56df8d1fd7d8a
ArmfeterLex <alexromanenkobeats@yandex.ru>
2026-03-01 18:37:32 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WindowsFormsAppOS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.butCheckOS = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 36);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(304, 251);
+            this.listBox1.TabIndex = 0;
+            // 
+            // butCheckOS
+            // 
+            this.butCheckOS.BackColor = System.Drawing.Color.BlueViolet;
+            this.butCheckOS.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.butCheckOS.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.butCheckOS.Location = new System.Drawing.Point(12, 293);
+            this.butCheckOS.Name = "butCheckOS";
+            this.butCheckOS.Size = new System.Drawing.Size(304, 25);
+            this.butCheckOS.TabIndex = 1;
+            this.butCheckOS.Text = "Определить ОС";
+            this.butCheckOS.UseVisualStyleBackColor = false;
+            this.butCheckOS.Click += new System.EventHandler(this.butCheckOS_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(292, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(25, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "×";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsAppOS.Properties.Resources.й;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(329, 330);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.butCheckOS);
+            this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Определение ОС";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button butCheckOS;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace WindowsFormsAppOS
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+            RoundButton(butCheckOS, 15);
+            RoundButton(button1, 15);
+            OperatingSystem os = Environment.OSVersion;
+            listBox1.Items.Add(os.Version);
+            listBox1.Items.Add(os.Platform);
+            listBox1.Items.Add(os.ServicePack);
+            listBox1.Items.Add(os.VersionString);
+        }
+
+        private void RoundButton(Button button, int radius)
+        {
+            Rectangle rect = new Rectangle(0, 0, button.Width, button.Height);
+            button.Region = new Region(CreateRoundedRectanglePath(rect, radius));
+        }
+
+        private GraphicsPath CreateRoundedRectanglePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            radius = Math.Min(radius, Math.Min(rect.Width / 2, rect.Height / 2));
+            path.AddArc(rect.X, rect.Y, radius, radius, 180, 90);
+            path.AddArc(rect.X + rect.Width - radius, rect.Y, radius, radius, 270, 90);
+            path.AddArc(rect.X + rect.Width - radius, rect.Y + rect.Height - radius, radius, radius, 0, 90);
+            path.AddArc(rect.X, rect.Y + rect.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+            return path;
+        }
+
+        private void CheckOSVersion()
+        {
+            OperatingSystem os = Environment.OSVersion;
+            Version version = os.Version;
+            if ((version.Major == 5 && version.Minor == 1) || version.Major >= 6)
+            {
+                MessageBox.Show("Программа может запускаться" + " в вашей операционной системе");
+            }
+            else
+            {
+                MessageBox.Show("Эта версия операционной системы не поддерживается." + "\r\n Используйте Windows XP или Windows Vista");
+            }
+        }
+
+        private void butCheckOS_Click(object sender, EventArgs e)
+        {
+            CheckOSVersion();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
